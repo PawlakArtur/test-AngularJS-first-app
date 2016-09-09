1178c7681765c96c79a7a222c42a4fb2a7bc9135
Artur Pawlak <pawlak.artur90@gmail.com>
2016-09-09 14:38:36 +0200

    working on new thing like ngAnimate, ng-if, ig-show/Hide, ng-include etc

--- app/controllers/directory.js
+++ app/controllers/directory.js
@@ -1,12 +1,14 @@
-angular.module('directoryApp', [])
+angular.module('directoryApp', ['ngAnimate'])
     .controller('directoryController', function() {
         var dirList = this;
 
+        dirList.toggle = true;
+
         dirList.list = [
-            {name:'Scott', age: 30},
-            {name:'Artur', age: 11},
-            {name:'Maciek', age: 14},
-            {name:'Stefan', age: 21}
+            {name:'Scott', age: 34, img: 'https://s3.amazonaws.com/uifaces/faces/twitter/brad_frost/128.jpg'},
+            {name:'Artur', age: 11, img: 'https://s3.amazonaws.com/uifaces/faces/twitter/jsa/128.jpg'},
+            {name:'Maciek', age: 14, img: 'https://s3.amazonaws.com/uifaces/faces/twitter/brad_frost/128.jpg'},
+            {name:'Stefan', age: 21, img: 'https://s3.amazonaws.com/uifaces/faces/twitter/sauro/128.jpg'}
         ];
 
         dirList.addPerson = function() {
